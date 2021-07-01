e707d2d599e56b8a64dd11c5dc4294876e38d30f
Road-Drum <haziqsembilanlima@gmail.com>
2021-07-01 12:06:28 +0800

    Split state results

---
+++ vaccination/vax-per-state/split-per-state.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from enum import Enum
+
+data = pd.read_csv("../vax_state.csv")
+
+states = ['Johor','Kedah','Kelantan','Melaka',
+'Negeri Sembilan','Pahang','Perak','Perlis','Pulau Pinang',
+'Sabah','Sarawak','Selangor','Terengganu','W.P. Kuala Lumpur',
+'W.P. Labuan','W.P. Putrajaya']
+
+for state in states:
+ filtered = data[data.state.str.contains(state)]
+ filtered.to_csv(state+".csv")
+
+
